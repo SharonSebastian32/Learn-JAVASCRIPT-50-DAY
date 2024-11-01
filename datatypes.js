1a1575e05e58a0010c552089ba79ae7256beb5f6
Sharon Sebastian <sharonsebastian81@gmail.com>
2024-11-01 22:10:09 +0530

    day 2 datatypes

---
+++ datatypes.js
@@ -0,0 +1,32 @@
+// "use strict"; //treat all js code as newer version
+
+// make code readable
+
+/* Always make code readability */
+
+let name = "sharon";
+let phoneNumber = 8589914973;
+let isGraduated = true;
+
+//number   range => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value  representation of
+//undefined =>
+//symbol => unique
+//object
+
+let age;
+let time = null;
+
+console.table(typeof name);
+console.table(typeof phoneNumber);
+console.table(typeof isGraduated);
+console.table(typeof age);
+console.table(typeof time);
+
+console.log(typeof undefined); // type of undefined is undefined
+console.log(typeof null); // type of null is object
+ 
+
